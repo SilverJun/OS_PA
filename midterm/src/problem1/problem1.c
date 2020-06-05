bf78dceed6a7956f255e0f12ccab9c18672daa1d
SilverJun <jangej1031@naver.com>
2020-06-05 11:47:29 +0900

    problem1 done.

--- midterm/src/problem1/problem1.c
+++ midterm/src/problem1/problem1.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 /* TODO */
+
+int pipes[2] ;
 
 char * 
 read_exec (char * exe)
 {
-/* TODO*/
+	pid_t child_pid ;
+	
+	if (pipe(pipes) != 0) {
+		perror("Error") ;
+		exit(1) ;
+	}
+	
+	child_pid = fork() ;
+	if (child_pid == 0) {
+		close(pipes[0]);
+		dup2(pipes[1], 1 /* STDOUT*/) ;
+		execl(exe, exe, 0x0);
+		exit(0);
+	}
+	else {
+		wait(0x0);
+		close(pipes[1]);
+		char* buf = (char*)malloc(sizeof(char) * 2048);
+		// get string from pipe and print that.
+		ssize_t s ;
+		while ((s = read(pipes[0], buf, 2047)) > 0) {
+			buf[s + 1] = 0x0 ;
+			//printf(">%s\n", buf) ;
+		}
+		return buf;
+	}
 }
 
 int 
